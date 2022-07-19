21d145c814ed18808936cfc81c3fa40f32bcde48
cmon-theres-gotta-be-a-name-available <ben.ridgeway9@gmail.com>
2022-07-19 15:29:43 -0400

    added employee questions and html template

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+
+
+
 const { generatePage } = require("./Develop/utils/generateHTML.js");
-console.log(inquirer);
-console.log(fs);
+
 
 function questions() {
 
